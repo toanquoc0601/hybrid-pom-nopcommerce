4f19d8b7d798743f419517a0648a576dfaba929c
toanquoc0601 <toanquoc0601@gmail.com>
2022-10-10 17:12:02 +0700

    Demo_CMCGlobal

--- testcases/Google_Chrome/Search_With_GoogleChrome.java
+++ testcases/Google_Chrome/Search_With_GoogleChrome.java
@@ -21,6 +21,7 @@ public class Search_With_GoogleChrome {
 	WebElement element;
 	public String valueSearch = "Demo with selenium";
 	public String searchTextbox = "//input[@title='Tìm kiếm']";
+
 	@BeforeClass
 	public void beforeClass() {
 		String projectPath = System.getProperty("user.dir");
@@ -29,32 +30,35 @@ public class Search_With_GoogleChrome {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
+
 	@Test
 	public void TC_01_Search_And_Verify_Google_With_Submit() {
-		
+
 		driver.get("https://www.google.com.vn/");
-		
+
 		waitToElementVisuble(driver, searchTextbox);
-		
+
 		highlightElement(driver, searchTextbox);
-		
+
 		getElement(driver, searchTextbox).sendKeys(valueSearch);
-		
+
 		getElement(driver, searchTextbox).submit();
-		
+
 		Assert.assertEquals(getElement(driver, "//input[@role='combobox']").getAttribute("value"), valueSearch);
 		
-		Assert.assertTrue(getElement(driver, "//div[@id='search']//h3[text()='Demo with selenium']").isDisplayed());
+		Assert.assertTrue(getElement(driver, "//div[@id='search']//h3[text()= '" + valueSearch + "']").isDisplayed());
+
 	}
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
-	
+
 	public WebElement getElement(WebDriver driver, String locator) {
 		return driver.findElement(By.xpath(locator));
 	}
-	
+
 	public void highlightElement(WebDriver driver, String locator) {
 		jsExecutor = (JavascriptExecutor) driver;
 		element = driver.findElement(By.xpath(locator));
@@ -65,6 +69,7 @@ public class Search_With_GoogleChrome {
 		jsExecutor.executeScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style",
 				originalStyle);
 	}
+
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000);
@@ -72,9 +77,10 @@ public class Search_With_GoogleChrome {
 			e.printStackTrace();
 		}
 	}
+
 	public void waitToElementVisuble(WebDriver driver, String locator) {
 		explicitWait = new WebDriverWait(driver, 30);
 		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
 	}
-	
+
 }
